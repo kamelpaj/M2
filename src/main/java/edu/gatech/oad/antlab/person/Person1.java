3bf79abd4178b9255cd7b79bf4a62aa0721fcbc6
Hatsune Morioka <hatsune.morioka@gatech.edu>
2019-09-16 11:55:25 -0400

    finished person1 class

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -30,8 +30,14 @@ public class Person1 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
+		//Person 1 put your implementation here
+		String s = "";
+		for (int i = 2; i < input.length(); i++) {
+			s += input.charAt(i);
+		}
+		s += input.charAt(0);
+		s += input.charAt(1);
+		return s;
 	}
 	
 	/**
